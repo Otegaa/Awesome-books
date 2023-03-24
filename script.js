1704e71e2dc838f5688c0555db28fbc0403a44cd
otegaa <ekiugbotega@gmail.com>
2023-03-24 08:53:37 +0100

    Implement SPA for website

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+const date = document.querySelector('.date');
+const time = document.querySelector('.time');
+const dateAndTime = document.querySelector('.date-input');
+const displayedBooks = document.querySelector('.list');
+const addBook = document.querySelector('.add');
+const contact = document.querySelector('.contact');
+const listPage = document.querySelector('.list-page');
+const addPage = document.querySelector('.add-page');
+const contactPage = document.querySelector('.contact-page');
+const navHeading = document.querySelector('.nav-heading');
+
 class AwesomeBooks {
   constructor() {
     this.title = document.querySelector('.book-title');
@@ -80,3 +91,51 @@ class AwesomeBooks {
 
 const books = new AwesomeBooks();
 books.init();
+
+date.innerHTML = new Date().toDateString();
+time.innerHTML = new Date().toLocaleTimeString();
+dateAndTime.innerHTML = `${date.innerHTML}, ${time.innerHTML}`;
+
+addBook.style.display = 'none';
+contact.style.display = 'none';
+listPage.classList.add('active');
+
+listPage.addEventListener('click', (e) => {
+  e.preventDefault();
+  displayedBooks.style.display = 'block';
+  addBook.style.display = 'none';
+  contact.style.display = 'none';
+  listPage.classList.add('active');
+  addPage.classList.remove('active');
+  contactPage.classList.remove('active');
+});
+
+addPage.addEventListener('click', (e) => {
+  e.preventDefault();
+  addBook.style.display = 'block';
+  displayedBooks.style.display = 'none';
+  contact.style.display = 'none';
+  addPage.classList.add('active');
+  listPage.classList.remove('active');
+  contactPage.classList.remove('active');
+});
+
+contactPage.addEventListener('click', (e) => {
+  e.preventDefault();
+  contact.style.display = 'block';
+  displayedBooks.style.display = 'none';
+  addBook.style.display = 'none';
+  contactPage.classList.add('active');
+  listPage.classList.remove('active');
+  addPage.classList.remove('active');
+});
+
+navHeading.addEventListener('click', (e) => {
+  e.preventDefault();
+  displayedBooks.style.display = 'block';
+  addBook.style.display = 'none';
+  contact.style.display = 'none';
+  listPage.classList.add('active');
+  addPage.classList.remove('active');
+  contactPage.classList.remove('active');
+});
